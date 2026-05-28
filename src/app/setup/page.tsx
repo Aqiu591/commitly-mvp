d1a3682fe850a4a609d31160b853a6ff523c03df
qiu a <2761384424@qq.com>
2026-05-28 15:29:27 +0800

    Make setup page read runtime env

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -1,5 +1,7 @@
 import { SetupChecklist } from "@/components/setup/setup-checklist";
 
+export const dynamic = "force-dynamic";
+
 export default function SetupPage() {
   return <SetupChecklist />;
 }
